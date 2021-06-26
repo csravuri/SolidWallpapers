6b043125f6a9d9a90c41137292c1cbb574827735
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2021-06-26 19:25:15 +0530

    added colour picker, hide/show feature and full screen mode for screenshot

---
+++ SolidWallpapers/SolidWallpapers/SolidWallpapers/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SolidWallpapers
+{
+    public partial class MainPage : ContentPage
+    {
+        public readonly BindableProperty IsPickerVisibleProperty = BindableProperty.Create(nameof(IsPickerVisible), typeof(bool), typeof(bool), true, BindingMode.TwoWay);
+
+        public bool IsPickerVisible
+        {
+            get => (bool)GetValue(IsPickerVisibleProperty);
+            set => SetValue(IsPickerVisibleProperty, value);
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Screen_Tapped(object sender, EventArgs e)
+        {
+            IsPickerVisible = !IsPickerVisible;
+        }
+    }
+}
